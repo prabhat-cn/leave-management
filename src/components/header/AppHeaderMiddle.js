21899d309f2cc28dbe1cbcbed16b9d7328e7ddf2
gitcoder-23 <prabhatece21@gmail.com>
2021-07-08 20:43:01 +0530

    Code cleaned

--- src/components/header/AppHeaderMiddle.js
+++ src/components/header/AppHeaderMiddle.js
@@ -8,31 +8,31 @@ const AppHeaderMiddle = () => {
     <>
       <CHeaderNav className="d-none d-md-flex me-auto">
         <CNavItem>
-          <CNavLink to="/dashboard" component={NavLink} activeClassName="active">
+          {/* <CNavLink to="/dashboard" component={NavLink} activeClassName="active">
             Dashboard
-          </CNavLink>
+          </CNavLink> */}
         </CNavItem>
         <CNavItem>
-          <CNavLink href="#">Users</CNavLink>
+          {/* <CNavLink href="#">Users</CNavLink> */}
         </CNavItem>
         <CNavItem>
-          <CNavLink href="#">Settings</CNavLink>
+          {/* <CNavLink href="#">Settings</CNavLink> */}
         </CNavItem>
       </CHeaderNav>
       <CHeaderNav>
         <CNavItem>
           <CNavLink href="#">
-            <CIcon name="cil-bell" size="lg" />
+            {/* <CIcon name="cil-bell" size="lg" /> */}
           </CNavLink>
         </CNavItem>
         <CNavItem>
           <CNavLink href="#">
-            <CIcon name="cil-list" size="lg" />
+            {/* <CIcon name="cil-list" size="lg" /> */}
           </CNavLink>
         </CNavItem>
         <CNavItem>
           <CNavLink href="#">
-            <CIcon name="cil-envelope-open" size="lg" />
+            {/* <CIcon name="cil-envelope-open" size="lg" /> */}
           </CNavLink>
         </CNavItem>
       </CHeaderNav>
