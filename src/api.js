1855a796e0615ee360328e7a5fe00da8b07c18ec
gitcoder-23 <prabhatece21@gmail.com>
2021-09-14 10:33:12 +0530

    Fresh Commit

--- src/api.js
+++ src/api.js
@@ -12,29 +12,30 @@ axiosRetry(API, { retries: 3 })
 
 // Add a request interceptor here global function added
 API.interceptors.request.use((config) => {
-
   const userData = localStorage.getItem('lMuserDataToken')
   const currentUser = JSON.parse(userData)
 
-  config.headers.Authorization =  userData ? `Bearer ${currentUser.token}` : '';
+  config.headers.Authorization = userData ? `Bearer ${currentUser.token}` : ''
 
-  return config;
-});
-
+  return config
+})
 
 // after invalid token logout
-API.interceptors.response.use((config) => {
-  // console.log('config', config);
-  return config;
-}, (err) => {
-  // console.log('err', err.response, err.data, err.message)
-  // console.log('err', err.response.data.code)
-  if( err.response.data.code === 'jwt_auth_invalid_token' || err.response.status === 403 ){
-    toast.error('Error! Token Missmatched')
-    localStorage.removeItem('lMuserDataToken')
-    window.location.reload()
-    window.location.href = '/'
-  }
-});
+API.interceptors.response.use(
+  (config) => {
+    // console.log('config', config);
+    return config
+  },
+  (err) => {
+    // console.log('err', err.response, err.data, err.message)
+    // console.log('err', err.response.data.code)
+    if (err.response.data.code === 'jwt_auth_invalid_token' || err.response.status === 403) {
+      toast.error('Error! Token Missmatched')
+      localStorage.removeItem('lMuserDataToken')
+      window.location.reload()
+      window.location.href = '/'
+    }
+  },
+)
 
-export { API as default };
+export { API as default }
